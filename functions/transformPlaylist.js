256383b8d40f0c80e788b0f3b2cc3066196ac3d7
Amir Rasouli <arasouli91@gmail.com>
2022-11-10 21:23:06 +0000

    set up netlify

--- functions/transformPlaylist.js
+++ functions/transformPlaylist.js
@@ -1,5 +1,6 @@
 exports.handler = async function (event, context) {
     let resPlaylist = [];
+    console.log("INSIDE SERVERLESS FUNCTION")
     return {
         statusCode: 200,
         body: JSON.stringify(resPlaylist)
