a32d55b9ef3713f4fbc7654844cd83c1010d1d48
arasouli91 <arasouli91@gmail.com>
2022-11-10 13:36:19 -0800

    c

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Box } from '@mui/material';
 import {
   VideoDetail,
   Navbar,
-  Feed,
   Playlist
 } from './components';
 
@@ -18,8 +17,7 @@ const App = () => (
     >
       <Navbar />
       <Switch>
-        <Route exact path='/' component={Feed} />
-        <Route exact path='/playlist' component={Playlist} />
+        <Route exact path='/' component={Playlist} />
         <Route path='/video/:id' component={VideoDetail} />
       </Switch>
     </Box>
