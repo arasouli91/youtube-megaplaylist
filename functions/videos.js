459e85777c22a29f8937f93dfddcd8a705b47734
arasouli91 <arasouli91@gmail.com>
2022-11-13 17:09:30 -0800

    c

--- functions/videos.js
+++ functions/videos.js
@@ -2,7 +2,7 @@
 Retrieve videos
 If id is provided, then just retrieve that video
 */
-import fetch from "node-fetch";
+const fetch = require('node-fetch')
 const { MongoClient } = require("mongodb");
 
 const mongoClient = new MongoClient(process.env.REACT_APP_MONGODB_URI);
